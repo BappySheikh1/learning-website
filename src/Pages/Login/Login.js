097f0c43bfbae0df488d24486542f834947462b1
Your Name Main <you@example.com>
2022-10-25 07:15:33 -0700

    register page added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -10,25 +10,19 @@ const Login = () => {
     }
 
     return (
-        <div className='w-50 border p-4 mx-auto mt-5'>
-             <Form onSubmit={handleSubmit} className='w-50'>
+        <div className='w-50 border p-4 mx-auto mt-5 rounded'>
+             <Form onSubmit={handleSubmit} className='w-75'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Control type="email" name='name' placeholder="Enter email" />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" name='password' placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Button  variant="primary" type="submit">
+        Log In
       </Button>
     </Form>
         </div>
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Register = () => {
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+ 
+     }
+
     return (
         <div>
-            <h3>This Is Register page</h3>
+            <Form onSubmit={handleSubmit} className='w-75'>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+         <Form.Group className="mb-3" controlId="formBasicCheckbox">
+           <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+
+            <Form.Text className="text-muted">
+             {}
+            </Form.Text>
+           <Button variant="primary" type="submit">
+            Submit
+          </Button>
+         </Form>
         </div>
     );
 };
