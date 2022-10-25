72f30f3a557950d46dc53f4cabf65a4a27783792
Your Name Main <you@example.com>
2022-10-25 11:08:17 -0700

    registered funcitonaly added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -6,6 +6,10 @@ const Login = () => {
 
     const handleSubmit=(event)=>{
        event.preventDefault();
+       const form=event.target
+       const email=form.email.value
+       const password=form.password.value
+       console.log(email,password);
 
     }
 
@@ -14,12 +18,12 @@ const Login = () => {
              <Form onSubmit={handleSubmit} className='w-75'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name='name' placeholder="Enter email" />
+        <Form.Control type="email" name='email' placeholder="Enter email" required />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name='password' placeholder="Password" />
+        <Form.Control type="password" name='password' placeholder="Password" required/>
       </Form.Group>
       <Button  variant="primary" type="submit">
         Log In
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -6,13 +6,19 @@ const Register = () => {
 
     const handleSubmit=(event)=>{
         event.preventDefault();
+        const form=event.target
+        const fullName=form.name.value
+        const photoURL=form.photoURL.value
+       const email=form.email.value
+       const password=form.password.value
+       console.log( fullName,photoURL,email,password);
  
      }
 
     return (
         <div>
             <Form onSubmit={handleSubmit} className='w-75'>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
         
