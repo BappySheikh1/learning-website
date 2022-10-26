b77bcb1624c8bc8c9b48d7402f48c84f73b06c57
Your Name Main <you@example.com>
2022-10-26 07:12:47 -0700

    cardDetails modify added

--- src/Pages/SummaryCard/SummaryCard.js
+++ src/Pages/SummaryCard/SummaryCard.js
@@ -3,14 +3,14 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
-import { FaDownload,FaCartPlus } from 'react-icons/fa';
+import { FaDownload,FaCartPlus, FaStar, FaEye } from 'react-icons/fa';
 
 const SummaryCard = ({ct}) => {
-    
-    const {title,description,image,id,author}=ct
+    console.log(ct);
+    const {title,description,image,id,author,rating}=ct
     return (
         <div>
-            <Card style={{width:'700px'}} className="mx-auto mt-4">
+            <Card style={{width:'70%'}} className="mx-auto my-4">
       <Card.Header className='d-flex justify-content-between align-items-center'>
         <div className='d-flex align-items-center'>
           <div>
@@ -40,8 +40,16 @@ const SummaryCard = ({ct}) => {
         </Card.Text>
         <Link to='/courses'><Button variant="primary">Go To Courses</Button></Link>
       </Card.Body>
-      <Card.Footer className="text-muted">
-        
+
+      <Card.Footer className="d-flex justify-content-between px-5 p-3"> 
+        <div>
+        <FaStar  className='icons-react text-warning'/>
+        <span className='ms-1'>{rating?.rate}</span>
+        </div>
+        <div >
+         <FaEye className='icons-react'/>
+         <span>{rating?.count}</span>
+        </div>
       </Card.Footer>
     </Card>
         </div>
